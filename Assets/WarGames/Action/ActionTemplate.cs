370ee928173c32c293fbeefca5c5b92b09c184b5
ShaneNalezyty <shane.nalezyty@HU130C05.Blackburn.College>
2016-02-19 12:29:32 -0600

    uml

--- Assets/WarGames/Action/ActionTemplate.cs
+++ Assets/WarGames/Action/ActionTemplate.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
-
+/// <summary>
+/// The Action namespace contains the interface action and all classes that implement it.
+/// </summary>
 namespace WarGames.Action {
+    /// <summary>
+    /// Interface all actions must implement 
+    /// </summary>
     public class ActionTemplate {
 
         private ParagonAI.BaseScript baseScript;
@@ -14,11 +19,16 @@ namespace WarGames.Action {
             baseScript = script;
             target = targetTransform;
         }
-
+        /// <summary>
+        /// All Actions need a AI cycle with run time logic in it.
+        /// </summary>
+        /// <returns>true if this cycle completed the action.</returns>
         public bool AICycle() {
             return true;
         }
-
+        /// <summary>
+        /// Ran on the distruction of a action.
+        /// </summary>
         public void OnEndAction() {
 
         }
